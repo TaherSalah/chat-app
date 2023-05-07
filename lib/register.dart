7f2ca0d70d98909435d68c14dd3946aa0fd4fd88
TaherSalah <tahersalah2016@gmail.com>
2023-05-07 22:56:59 +0200

    add modal indecator HUD packge in regesterion

--- lib/register.dart
+++ lib/register.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegisterScreen extends StatefulWidget {
   RegisterScreen({Key? key}) : super(key: key);
@@ -14,126 +15,136 @@ class _RegisterScreenState extends State<RegisterScreen> {
   String? email;
   String? password;
   GlobalKey<FormState> formKey = GlobalKey();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(child: Image.asset(height: 150, 'assets/images/login.png')),
-            const Text(
-              'Register Now',
-              style: TextStyle(fontSize: 25),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                onChanged: (data) {
-                  email = data;
-                },
-                validator: (value) {
-                  if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value!)) {
-                    return 'Please enter a valid email address';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.email_outlined),
-                    hintText: 'Enter Your Email',
-                    label: Text('Email'),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    )),
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                  child: Image.asset(height: 150, 'assets/images/login.png')),
+              const Text(
+                'Register Now',
+                style: TextStyle(fontSize: 25),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextFormField(
-                obscureText: widget.isActive,
-                onChanged: (data) {
-                  password = data;
-                },
-                validator: (value) {
-                  if (value!.trim().length < 8) {
-                    return 'Password must be at least 8 characters in length';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                    suffixIcon: InkWell(
-                      onTap: togglePassword,
-                      child: widget.isActive
-                          ? const Icon(Icons.visibility_off)
-                          : const Icon(Icons.visibility),
-                    ),
-                    prefixIcon: const Icon(Icons.lock_open_outlined),
-                    hintText: 'Enter Your Password',
-                    label: const Text('password'),
-                    focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green),
-                    ),
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    )),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Are You Have Account ? '),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, 'Login');
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  onChanged: (data) {
+                    email = data;
                   },
-                  child: const Text(
-                    'Login Now',
-                  ),
-                ),
-              ],
-            ),
-            ElevatedButton(
-                style: const ButtonStyle(
-                    backgroundColor:
-                        MaterialStatePropertyAll(Colors.deepPurple),
-                    elevation: MaterialStatePropertyAll(10),
-                    textStyle: MaterialStatePropertyAll(
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                    padding: MaterialStatePropertyAll(
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 40))),
-                onPressed: () async {
-                  if (formKey.currentState!.validate()) {
-                    try {
-                      await authEmailAndPass();
-
-                      print(authEmailAndPass().user.user!.metadata);
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'week-password') {
-                        showSnakBar(context,
-                            title: 'The Password Provided is Week',
-                            bgColor: Colors.amber);
-                      } else if (e.code == 'email-already-in-use') {
-                        showSnakBar(context,
-                            title:
-                                'The email address is already in use by another account.!!',
-                            bgColor: Colors.redAccent);
-                      }
-                      print(e.toString());
-                    } catch (e) {
-                      print(e.toString());
+                  validator: (value) {
+                    if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value!)) {
+                      return 'Please enter a valid email address';
                     }
-                  }
-                },
-                child: const Text('Register'))
-          ],
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.email_outlined),
+                      hintText: 'Enter Your Email',
+                      label: Text('Email'),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      )),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  obscureText: widget.isActive,
+                  onChanged: (data) {
+                    password = data;
+                  },
+                  validator: (value) {
+                    if (value!.trim().length < 8) {
+                      return 'Password must be at least 8 characters in length';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                      suffixIcon: InkWell(
+                        onTap: togglePassword,
+                        child: widget.isActive
+                            ? const Icon(Icons.visibility_off)
+                            : const Icon(Icons.visibility),
+                      ),
+                      prefixIcon: const Icon(Icons.lock_open_outlined),
+                      hintText: 'Enter Your Password',
+                      label: const Text('password'),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      )),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Are You Have Account ? '),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'Login');
+                    },
+                    child: const Text(
+                      'Login Now',
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                  style: const ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.deepPurple),
+                      elevation: MaterialStatePropertyAll(10),
+                      textStyle: MaterialStatePropertyAll(
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                      padding: MaterialStatePropertyAll(
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 40))),
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      try {
+                        isLoading = true;
+                        setState(() {});
+                        await authEmailAndPass();
+                        Navigator.pushNamed(context, 'Login');
+
+                        print(authEmailAndPass().user.user!.metadata);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'week-password') {
+                          showSnakBar(context,
+                              title: 'The Password Provided is Week',
+                              bgColor: Colors.amber);
+                        } else if (e.code == 'email-already-in-use') {
+                          showSnakBar(context,
+                              title:
+                                  'The email address is already in use by another account.!!',
+                              bgColor: Colors.redAccent);
+                        }
+                        print(e.toString());
+                      } catch (e) {
+                        print(e.toString());
+                      }
+                      isLoading = false;
+                      setState(() {});
+                    }
+                  },
+                  child: const Text('Register'))
+            ],
+          ),
         ),
       ),
     );
